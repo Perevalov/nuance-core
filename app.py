f4e5d1a4d8d769646183855f062d7b83f87b866a
XrenynTheMage <thomas.fritz.2000@gmx.de>
2019-11-10 23:20:20 +0100

    Reimplemented Alexandr's way of generating the response message

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ def get_cities_for_a_text(text):
 @app.route("/get_answer", methods=['GET'])
 def get_answer():
     print("get_answer Started")
-    question_text = request.form.get("question_text")
+    question_text = request.form.get("user_text")
     supported_question_prefix = "How many people live in "
     cities = get_cities_for_a_text(question_text)
-    results: json
+    response_array = list()
 
     # TODO the following if statement shall be replaced with a more sophisticated question answering algorithm
     if question_text.lower().strip().startswith(supported_question_prefix.lower()) and len(cities) > 0:
@@ -53,19 +53,21 @@ def get_answer():
         """ % (cities[0].lower()))  # only searches for the very first city mentioned
 
         sparql.setReturnFormat(JSON)
-        results = json.dump(sparql.query().convert())
+        for result in results["results"]["bindings"]:
+            response_array.append({'City Label': result['cityLabel']['value'],
+                                   'Num. of Inhabitants': result['numberOfInhabitants']['value']})
 
     else:
         # TODO: improve error handling for unsupported questions (if they exist)
-        results = json.dump(
-            {'Question \"%s\" is not supported.' % question_text:
-                'Only questions starting with the phrase \"%s\" and containing a city name might be evaluated.'
-                    % supported_question_prefix}
+        response_array.append(
+            {'Question \"%s\" is not supported.'
+             % question_text: 'Only questions starting with the phrase \"%s\" and containing a city name might be evaluated.'
+                              % (supported_question_prefix)}
         )
-
+    session['response'] = response_array
     print("get_answer Ended")
-    return results
+    return session
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=6000)
+    app.run(debug=True, host='127.0.0.1', port=5001)
