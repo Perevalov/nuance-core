661dfd1799b2a2712220c58c101a49c6f3f56152
Перевалов Александр Максимович <perevalova@pstu.ru>
2019-12-15 18:14:10 +0100

    Fixed docker compose

--- boot.sh
+++ boot.sh
@@ -9,6 +9,26 @@ then
   then
     exec gunicorn -b :5001 --access-logfile - --error-logfile - --chdir /home/nuance-core/qanary_components/template_classifier app:app
     exit
+  elif [ $SERVICE_TYPE == "question_validator" ]
+  then
+    exec gunicorn -b :5003 --access-logfile - --error-logfile - --chdir /home/nuance-core/qanary_components/question_validator app:app
+    exit
+  elif [ $SERVICE_TYPE == "relation_classifier" ]
+  then
+    exec gunicorn -b :5004 --access-logfile - --error-logfile - --chdir /home/nuance-core/qanary_components/relation_classifier app:app
+    exit
+  elif [ $SERVICE_TYPE == "sparql_builder" ]
+  then
+    exec gunicorn -b :5005 --access-logfile - --error-logfile - --chdir /home/nuance-core/qanary_components/sparql_builder app:app
+    exit
+  elif [ $SERVICE_TYPE == "sparql_worker" ]
+  then
+    exec gunicorn -b :5006 --access-logfile - --error-logfile - --chdir /home/nuance-core/qanary_components/sparql_worker app:app
+    exit
+  elif [ $SERVICE_TYPE == "template_generator" ]
+  then
+    exec gunicorn -b :5007 --access-logfile - --error-logfile - --chdir /home/nuance-core/qanary_components/template_generator app:app
+    exit
   fi
 else
   echo -e "SERVICE_TYPE not set\n"
